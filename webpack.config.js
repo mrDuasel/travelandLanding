90d2a685f4b6c2f4b48a652b997434b9b112eb64
Alexandr Kopelev <mr.duasel@gmail.com>
2021-08-09 21:01:17 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.pug'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets') , to: path.resolve(__dirname, 'dist/assets')
+                }
+            ]
+        }),
+    ],
+    module:{
+        rules: [
+            
+           {
+            test: /\.s?[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader'],
+            },
+    
+            {
+                test: /\.pug$/,
+                    loader: 'pug-loader'
+            },
+          
+    
+        ]
+    }
+}
